a3d9f2966ca1be1c307832df68d9e72aa67ca067
Marcin Marszewski <marcin.marszewski1@outlook.com>
2025-08-01 13:28:41 +0200

    FishingSpotDTO change

--- backend/src/main/java/pl/aleokaz/backend/fishingspot/FishingSpotDTO.java
+++ backend/src/main/java/pl/aleokaz/backend/fishingspot/FishingSpotDTO.java
@@ -2,9 +2,6 @@ package pl.aleokaz.backend.fishingspot;
 
 import lombok.Builder;
 import lombok.NonNull;
-import pl.aleokaz.backend.post.PostDTO;
-
-import java.util.List;
 import java.util.UUID;
 
 @Builder
@@ -14,6 +11,5 @@ public record FishingSpotDTO(
         String description,
         @NonNull UUID ownerId,
         double longitude,
-        double latitude,
-        @NonNull List<PostDTO> posts) {
+        double latitude) {
 }
