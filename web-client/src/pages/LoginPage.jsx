2e5419683fd7c059265c5890753215b6b76d4a1f
Marcin Marszewski <marcin.marszewski1@outlook.com>
2025-08-08 18:53:42 +0200

    User component display fix & login disclaimer

--- web-client/src/pages/LoginPage.jsx
+++ web-client/src/pages/LoginPage.jsx
@@ -4,6 +4,7 @@ export default function LoginPage() {
     return (
         <div className="login-page-container">
             <LoginComponent />
+            <div>Website currently works best on dekstop browsers</div>
         </div>
     );
 }
